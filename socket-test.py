7bf75048cc16f1d9adfa49102d4b64ca88b9bd19
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2026-07-03 10:16:49 +0100

    socket seems to be far more reliable

--- socket-test.py
+++ socket-test.py
@@ -7,7 +7,7 @@ MEDIAN_RSSI = dict()
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", 8000))
 
-names = ("eduroam", "UAL-IoT", "UAL-WiFi", "UAL-Guest-WiFi")
+names = ("eduroam", "UAL-IoT", "UAL-WiFi", "UAL-Guest-WiFi", "Igloo")
 
 print("Listening...")
 
@@ -20,7 +20,7 @@ def calculate_median(vals):
 
 counter = 0
 
-while counter < 150:
+while counter < 200:
 
     data, addr = sock.recvfrom(2048)
     data = data.decode()
@@ -30,16 +30,23 @@ while counter < 150:
     for key in d.keys():
         if d[key]["name"] not in names:
             continue
-        if key not in RSSI_VALUES:
-            RSSI_VALUES[key] = []
-        RSSI_VALUES[key].append(d[key]["rssi"])
+
+        combined_name = f"{d[key]['name']}-{key}"
+        if combined_name not in RSSI_VALUES:
+            RSSI_VALUES[combined_name] = []
+
+        if len(RSSI_VALUES[combined_name]) < 50:
+            RSSI_VALUES[combined_name].append(d[key]["rssi"])
 
     counter += 1
-    if len(RSSI_VALUES) < 10 and not all(
-        [len(vals) >= 50 for vals in RSSI_VALUES.values()]
+    if len(RSSI_VALUES) > 10 and all(
+        [len(vals) == 50 for vals in RSSI_VALUES.values()]
     ):
         break
 
-MEDIAN_RSSI = {key: calculate_median(RSSI_VALUES[key]) for key in RSSI_VALUES}
-print(MEDIAN_RSSI)
+MEDIAN_RSSI = {key: calculate_median(RSSI_VALUES[key]) for key in RSSI_VALUES if len(RSSI_VALUES[key]) == 50}
+
+for key in MEDIAN_RSSI:
+    print(f"{key}: {MEDIAN_RSSI[key]}")
+
 exit()
