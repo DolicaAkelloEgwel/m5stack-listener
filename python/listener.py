d9aff59b29d99c50a2c5a49e1da1ba9a7ff21d26
DolicaAkelloEgwel <d.egwel@gmail.com>
2026-07-10 11:42:07 +0100

    moving python stuff to its own folder

---
+++ python/listener.py
@@ -0,0 +1,71 @@
+import json
+import socket
+from pythonosc import udp_client
+
+RSSI_VALUES = dict()
+
+# create a socket for listening to M5Stick messages
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("0.0.0.0", 8000))
+print("Listening...")
+
+# track names of nearby wifi networks
+WIFI_NAMES = ("eduroam", "UAL-IoT", "UAL-WiFi", "UAL-Guest-WiFi", "Igloo")
+
+MAX_RECORDINGS = 50
+MIN_NETWORKS_NEEDED = 10
+
+
+def calculate_median(vals):
+
+    ordered = sorted(vals)
+    n = len(vals)
+
+    if n % 2 == 0:
+        mid_index = n // 2
+        return (ordered[mid_index - 1] + ordered[mid_index]) / 2
+    else:
+        return ordered[(n // 2)]
+
+
+counter = 0
+
+while counter < 200:
+
+    data, addr = sock.recvfrom(2048)
+    data = data.decode()
+    d = json.loads(data)
+    print("Received data.")
+
+    for key in d.keys():
+        # continue if the network isn't in the list
+        if d[key]["name"] not in WIFI_NAMES:
+            continue
+
+        combined_name = f"{d[key]['name']}-{key}"
+
+        # create a new dictionary entry if this has not been seen before
+        if combined_name not in RSSI_VALUES:
+            RSSI_VALUES[combined_name] = []
+
+        # take 50 recordings
+        if len(RSSI_VALUES[combined_name]) < MAX_RECORDINGS:
+            RSSI_VALUES[combined_name].append(d[key]["rssi"])
+
+    counter += 1
+
+    # stop when 50 recordings have been taken for at least 10 networks
+    if (
+        sum([len(vals) == MAX_RECORDINGS for vals in RSSI_VALUES.values()])
+        > MIN_NETWORKS_NEEDED
+    ):
+        break
+
+    print(RSSI_VALUES)
+
+# calculate the median rssi value
+for key in RSSI_VALUES:
+    if len(RSSI_VALUES[key]) == 50:
+        print(f"{key}: {calculate_median(RSSI_VALUES[key])}")
+    else:
+        print(len(RSSI_VALUES[key]))
